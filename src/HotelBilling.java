3a06b4ce40d1f440760b33d8044645a81c0a4e02
martin2387 <cheenote@hotmail.com>
2013-08-16 13:01:26 -0400

    Holla at an Ops fan!!

--- src/HotelBilling.java
+++ src/HotelBilling.java
@@ -1,21 +1,64 @@
+import javax.swing.JOptionPane;
 
 public class HotelBilling {
+	
+	final static float SALES_TAX = 0.13f;
 
 	public static void main(String[] args) {
-		/*
-		 * In the main method, declare and initialize variables
-		 * for room price, number of rooms booked, and coupon rate.
-		 * 
-		 * Then call each case of the overloaded method 
-		 * calculateBill() to demonstrate the use of overloaded 
-		 * methods
-		 */
+		
+		//Declare value for objects
+		double roomRate = 99.00;
+		int bookedRooms = 3;
+		double coupon = 0.10;
+		double totalBill;
+		
+		totalBill = calculateBill(roomRate);
+		JOptionPane.showMessageDialog(null, "Your total bill is $" + totalBill);
+		
+		totalBill = calculateBill(roomRate, bookedRooms);
+		JOptionPane.showMessageDialog(null, "Your total with tax for " + bookedRooms + " rooms is $" + totalBill);
+		
+		totalBill = calculateBill(roomRate, bookedRooms, coupon);
+		JOptionPane.showMessageDialog(null, "Your total with tax for " + bookedRooms + " rooms is $ " + totalBill
+				+ "\nThat includes a discount of " + coupon * 100 + "%");
 	}
-
-	/*  
-	 * Here you will create four new methods:
-	 * -- Three cases of the overloaded method calculateBill()
-	 * -- A method called twoDecimalRounding() which will
-	 * 		be called by each case of the calculateBill() method.
-	 */
+	
+	//This method is to calculate the rate and tax 
+	public static double calculateBill(double rate) {
+		double tax = rate * SALES_TAX;
+		double finalRate = rate + tax;
+		finalRate = twoDecimalPlaces(finalRate);
+		return finalRate;		
+	}
+	
+	//This method is to calculate the rate, tax, and rooms a person would want
+	public static double calculateBill(double rate, int rooms) {
+		double tax = rate * SALES_TAX;
+		double finalRate = rate + tax;
+		finalRate = finalRate * rooms;
+		finalRate = twoDecimalPlaces(finalRate);
+		return finalRate;
+	}
+	
+	//This method is to calculate the rate, tax, rooms, with a discounted coupon
+	public static double calculateBill(double rate, int rooms, double coupon) {
+		double tax = rate * SALES_TAX;
+		double finalRate = rate + tax;
+		finalRate = finalRate * rooms;
+		double discount = coupon * finalRate;
+		finalRate = finalRate - discount;
+		finalRate = twoDecimalPlaces(finalRate);
+		return finalRate;
+	}
+	
+	//This method is to round 2 decimal places in final rate
+	public static double twoDecimalPlaces(double decimal) {
+		decimal = decimal * 100;
+		decimal = decimal + 0.5;
+		decimal = (int) decimal;
+		decimal = decimal / 100;
+		return decimal;
+	}
+				
 }
+
